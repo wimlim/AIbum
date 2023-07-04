ea7053d1e7763b3a371cf70708f57fa29e458c9c
Huang_Yuhan <huang_yuhan@sjtu.edu.cn>
2023-07-04 14:37:12 +0800

    fix bug : get the photos on login page

--- code/frontend/src/index.tsx
+++ code/frontend/src/index.tsx
@@ -20,6 +20,7 @@ const App:React.FC = ()=>{
   useEffect(
     ()=>
     {
+      if(window.location.pathname==="/login")return;
       let pictures:PhotoProps[]=[];
       getPhotos({}).then(
           (response)=>{
